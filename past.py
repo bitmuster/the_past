c30fff81203b64059a74af5a76f3ff5487aa59be
Michael Abel <mabel@bitmuster.org>
2019-03-09 23:55:28 +0100

    message from the past

--- past.py
+++ past.py
@@ -14,17 +14,19 @@ past = 1552172121 + 7
 
 filename = 'output.fortune'
 
-for i in range(10):
-    with open(filename, 'a') as myfile:
+for i in range(300):
+    for i in range(10):
+        with open(filename, 'a') as myfile:
 
-        for i in range(10):
-            with subprocess.Popen(["fortune"], stdout=subprocess.PIPE) as proc:
-                output = proc.stdout.read()
-                myfile.write(output.decode('ascii'))
+            for i in range(10):
+                with subprocess.Popen(["fortune"], stdout=subprocess.PIPE) as proc:
+                    output = proc.stdout.read()
+                    myfile.write(output.decode('ascii'))
 
-    os.system('git add *')
-    os.system('git commit --date %i -m\"message from the past\"'%past)
-    past += 1
+        os.system('git add *')
+        os.system('git commit --date %i -m\"message from the past\"'%past)
+        past += 1
+    past += 48*3600
 
 print(f'End time of the past is now {past}')
 
